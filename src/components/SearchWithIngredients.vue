0a0f62cf3731da27d8a6f27258fc2111b4742ac9
Ferdi Eraslan <frdiersln@hotmail.com>
2024-05-25 18:55:17 +0300

    +spoonacular request created for search recipes with ingredients

--- src/components/SearchWithIngredients.vue
+++ src/components/SearchWithIngredients.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 import { ingredients } from "../../stores/IngredientsStore.js"
+import axios from 'axios';
+
 
 let selectedIngredients = ref([])
 let visibleIngredients = ref([])
@@ -16,6 +18,26 @@ const search = query => { //search with loading
   } else {
     visibleIngredients.value = ingredients.slice(0, 26) // Show first 26 ingredients when search query is empty
   }
+}
+
+function searchRecipes() {
+  // Get selected ingredient labels from selected ingredient values
+  var selectedIngredientLabels = [];
+  for (let ingredientValue of selectedIngredients.value) {
+    selectedIngredientLabels.push(ingredients.filter(item => item.value === ingredientValue)[0].label)
+  }
+  console.log(selectedIngredientLabels)
+  // Call API to search recipes with selected ingredients
+  let apiKey = process.env.VUE_APP_SPOONACULAR_API_KEY;
+  let selectedIngredientLabelsString = selectedIngredientLabels.join(',')
+
+  axios.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + selectedIngredientLabelsString + '&number=2&apiKey=' + apiKey)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
 </script>
 
@@ -37,4 +59,8 @@ const search = query => { //search with loading
       :value="ingredient.value"
     />
   </el-select>
+
+  <el-button type="primary" @click="searchRecipes()">
+    Search
+  </el-button>
 </template>
